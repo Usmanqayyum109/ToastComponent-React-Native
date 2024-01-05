b2c840d4e422b8a8463da0d704676c370c6611a6
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2024-01-05 16:50:42 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
-import MainNavigation from './src/Navigation/MainNavigation';
 import messaging from '@react-native-firebase/messaging';
-import {persistedStore, store} from './src/Redux/Store';
-import {Provider} from 'react-redux';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {PersistGate} from 'redux-persist/integration/react';
-import {Colors} from './src/Constant/Colors';
-import {LogBox} from 'react-native';
 import {ToastComponent} from './src/Components/ToastComponent';
 
 export default function App() {
-  useEffect(() => {
-    LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
-    LogBox.ignoreAllLogs('Excessive number of pending callbacks: 501.');
-  }, []);
+  
 
   const [toastNotification, setToastNotification] = useState(false);
   const [notificationData, setNotificationData] = useState({});
@@ -37,14 +28,11 @@ export default function App() {
   }, [notificationData]);
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistedStore}>
+   
         <SafeAreaProvider>
           <StatusBar backgroundColor={Colors.black} barStyle={'default'} />
-          <MainNavigation />
           {toastNotification && <ToastComponent {...notificationData} />}
         </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
+     
   );
 }
