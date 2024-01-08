c54aab657b5c51240277e9b5a485f08926ae4acb
Usmanqayyum109 <108247694+Usmanqayyum109@users.noreply.github.com>
2024-01-08 11:27:47 +0500

    Update MainNavigation.js

--- MainNavigation.js
+++ MainNavigation.js
@@ -1,21 +1,13 @@
-import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import AuthNavigation from './AuthNavigation';
-import CommonNavigation from './CommonNavigation';
-import {useSelector} from 'react-redux';
-import {configAxiosHeaders} from '../API/ApiCalls';
 import SplashScreen from '../Screens/SplashScreen/SplashScreen';
 import NavigationSevice from './NavigationSevice';
 
 const mainStack = createNativeStackNavigator();
 
 function MainNavigation() {
-  const user = useSelector(state => state.UserReducer);
 
-  useEffect(() => {
-    configAxiosHeaders(user?.token);
-  }, [user]);
+ 
 
   return (
     <NavigationContainer
@@ -24,11 +16,7 @@ function MainNavigation() {
         initialRouteName={'SplashScreen'}
         screenOptions={{headerShown: false}}>
         <mainStack.Screen name="SplashScreen" component={SplashScreen} />
-        <mainStack.Screen name="AuthNavigation" component={AuthNavigation} />
-        <mainStack.Screen
-          name="CommonNavigation"
-          component={CommonNavigation}
-        />
+       
       </mainStack.Navigator>
     </NavigationContainer>
   );
